3fa98d5bcb945958ba253854cd390dc01e8bb084
felipeandree <felipeandre8@outlook.com>
2023-04-18 20:04:26 -0300

    update search bar

--- src/pages/Home/styles.jsx
+++ src/pages/Home/styles.jsx
@@ -1,9 +1,27 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
- 
-
-  color: red; 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1.5rem 1rem;
+  padding: 1rem;
 `;
 
+export const Logo = styled.div`
+  display: flex;
+`;
+
+export const LogoImg = styled.div`
+  width: 100%; 
+  display: flex;
+  justify-content: center;
+  > img {
+    width: 55%;
+    padding: 0
+    margin: 0;
+  } 
+`; 
+
 export default Container;
+
